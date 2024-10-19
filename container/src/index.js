2c63a159fc4621f4c9cac4d99f4c46a87683d8b4
Hariprasad <hariprasadk125@gmail.com>
2024-10-19 12:25:09 +0530

    added remote products

--- container/src/index.js
+++ container/src/index.js
@@ -1,12 +1,1 @@
-const text = document.getElementById('msg')
-const btn = document.getElementById('btn')
-let isRednow = false
-
-btn.addEventListener('click', ()=>{
-    if(!isRednow){
-        text.style.color = 'red'
-    }else{
-        text.style.color = 'black'
-    }
-    isRednow = !isRednow
-})
+console.log("Hiiii!!")
